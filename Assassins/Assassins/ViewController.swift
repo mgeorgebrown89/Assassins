001b73779d93a240437f67ad4d423ac7f2d31ba5
mgeorgebrown89 <m.george.brown89#gmail.com>
2018-03-07 11:29:25 -0800

    app skeleton basis

---
+++ Assassins/Assassins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assassins
+//
+//  Created by Michael Brown on 3/3/18.
+//  Copyright © 2018 The Elimination Framework. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
