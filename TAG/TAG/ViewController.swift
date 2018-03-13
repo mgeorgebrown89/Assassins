a330a3f617cf806d4431d1dbc31e5ae708d09cd9
mgeorgebrown89 <m.george.brown89#gmail.com>
2018-03-13 14:50:24 -0700

    minor edits

--- TAG/TAG/ViewController.swift
+++ TAG/TAG/ViewController.swift
@@ -9,60 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
-    var activeTextField: UITextField!
-    
-    //Register View Text Fields
-    @IBOutlet var firstNameTextField: UITextField!
-    @IBOutlet var lastNameTextField: UITextField!
-    @IBOutlet var usernameTextField: UITextField!
-    @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
-    @IBOutlet var phoneTextField: UITextField!
-    @IBOutlet var birthdayTextField: UITextField!
-    @IBOutlet var profileTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let center: NotificationCenter = NotificationCenter.default;
-        center.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-    }
-    
-    @objc func keyboardDidShow(notification: Notification) {
-        let info:NSDictionary = notification.userInfo! as NSDictionary
-        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-        let keyboardY = self.view.frame.size.height - keyboardSize.height
-        
-        let editingTextFieldY:CGFloat! = self.activeTextField?.frame.origin.y
-        
-        //Checking if the textfield is really hidden behind the keyboard
-        if editingTextFieldY > keyboardY - 60 {
-            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
-                self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y - (editingTextFieldY! - (keyboardY - 60)), width: self.view.bounds.width, height: self.view.bounds.height)
-            }, completion: nil)
-        }
-    }
-    
-    @objc func keyboardWillHide(notification: Notification) {
-        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
-            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-        }, completion: nil)
-        
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        activeTextField = textField
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
     override func didReceiveMemoryWarning() {
