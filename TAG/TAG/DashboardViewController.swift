1e47909b5e3f83b7a43d7a82ce68c5ce5af5178d
mgeorgebrown89 <m.george.brown89#gmail.com>
2018-05-16 11:35:20 -0700

    trying to figure out logging in to the dashboard

--- TAG/TAG/DashboardViewController.swift
+++ TAG/TAG/DashboardViewController.swift
@@ -8,56 +8,23 @@
 
 import UIKit
 
-struct Login: Decodable {
-    let PlayerID: Int
-    let FirstName: String
-    let LastName: String
-    let UserName: String
-    let Email: String
-    let Phone: String
-    let DOB: String
-    let PhotoUrl: String
-    let Profile: String
-    let AuthUserID: String
-}
 
 class DashboardViewController: UIViewController {
-    
+
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var profilePicImageView: UIImageView!
-    var imageURLString = " "
-    var email: String?
+    var imageURLString = "https://eliminationimages.blob.core.windows.net/ldskfj/ldskfj.jpeg"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let imageUrl = URL(string: imageURLString)
         
-        let jsonUrlString = "https://elimination.azurewebsites.net/api/Players/GetPlayerInfo?email=" + email!
+        let imageData = try! Data(contentsOf: imageUrl!)
         
-        guard let url = URL(string: jsonUrlString) else {return}
-        
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            //check err
-            //check response status 200 ok
-            
-            guard let data = data else {return}
-            
-            do {
-                let login = try JSONDecoder().decode(Login.self, from: data)
-                print(login)
-                self.userNameLabel.text = login.UserName
-                self.imageURLString = login.PhotoUrl
-                let imageUrl = URL(string: self.imageURLString)
-                let imageData = try! Data(contentsOf: imageUrl!)
-                self.profilePicImageView.image = UIImage(data: imageData)
-                
-                print(self.imageURLString)
-            } catch let jsonErr {
-                print("Error serializing json:", jsonErr)
-            }
-            
-            }.resume()
+        profilePicImageView.image = UIImage(data: imageData)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
