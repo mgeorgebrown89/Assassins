94d9c1d9619ad95d8145756262eb8be845780e6b
mgeorgebrown89 <m.george.brown89#gmail.com>
2018-05-16 07:20:03 -0700

    sort of successful loading of player values

--- TAG/TAG/DashboardViewController.swift
+++ TAG/TAG/DashboardViewController.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 
+struct Login: Decodable {
+    let PlayerID: Int
+    let FirstName: String
+    let LastName: String
+    let UserName: String
+    let Email: String
+    let Phone: String
+    let DOB: String
+    let PhotoUrl: String
+    let Profile: String
+    let AuthUserID: String
+}
 
 class DashboardViewController: UIViewController {
-
+    
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var profilePicImageView: UIImageView!
-    var imageURLString = "https://eliminationimages.blob.core.windows.net/ldskfj/ldskfj.jpeg"
+    var imageURLString = " "
+    var email: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let imageUrl = URL(string: imageURLString)
         
-        let imageData = try! Data(contentsOf: imageUrl!)
+        let jsonUrlString = "https://elimination.azurewebsites.net/api/Players/GetPlayerInfo?email=" + email!
         
-        profilePicImageView.image = UIImage(data: imageData)
+        guard let url = URL(string: jsonUrlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            //check err
+            //check response status 200 ok
+            
+            guard let data = data else {return}
+            
+            do {
+                let login = try JSONDecoder().decode(Login.self, from: data)
+                print(login)
+                self.userNameLabel.text = login.UserName
+                self.imageURLString = login.PhotoUrl
+                let imageUrl = URL(string: self.imageURLString)
+                let imageData = try! Data(contentsOf: imageUrl!)
+                self.profilePicImageView.image = UIImage(data: imageData)
+                
+                print(self.imageURLString)
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
+            }
+            
+            }.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
--- TAG/TAG/LoginViewController.swift
+++ TAG/TAG/LoginViewController.swift
@@ -8,18 +8,7 @@
 
 import UIKit
 
-struct Login: Decodable {
-    let PlayerID: Int
-    let FirstName: String
-    let LastName: String
-    let UserName: String
-    let Email: String
-    let Phone: String
-    let DOB: String
-    let PhotoUrl: String
-    let Profile: String
-    let AuthUserID: String
-}
+
 
 class LoginViewController: UIViewController {
     
@@ -27,50 +16,22 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginPasswordField: UITextField!
     
     var email: String?
-    var userName: String?
-    var photo: String?
+    var password: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    //move json stuff to the dashboard. just pass email from here
-    
-    func submitGet(email: String){
-        let jsonUrlString = "https://elimination.azurewebsites.net/api/Players/GetPlayerInfo?email=" + email
-        
-        guard let url = URL(string: jsonUrlString) else {return}
-        
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            //check err
-            //check response status 200 ok
-            
-            guard let data = data else {return}
-            
-            do {
-                let login = try JSONDecoder().decode(Login.self, from: data)
-                print(login)
-                self.userName = login.UserName
-                self.photo = login.PhotoUrl
-            } catch let jsonErr {
-                print("Error serializing json:", jsonErr)
-            }
-            
-            }.resume()
-    }
-    
     @IBAction func loginButtonPressed(_ sender: Any) {
         email = loginEmailField.text!
-        submitGet(email: email!)
+        password = loginPasswordField.text!
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//
-//        if let dashboardControllerVC = segue.destination as? DashboardViewController {
-//            dashboardControllerVC.userNameLabel?.text = userName
-//            dashboardControllerVC.imageURLString = photo!
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dashboardControllerVC = segue.destination as? DashboardViewController {
+            dashboardControllerVC.email = email
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
